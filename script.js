ce5a4a761c6bb470b1ed81bbbf1c8d41ab78c501
Ángelcoy <angelalbertogarcia1@gmail.com>
2026-09-25 22:38:30 +0000

    add all buttons of calculator to variables
    all number and operator buttons were added in variables and in two arrays

--- script.js
+++ script.js
@@ -24,8 +24,34 @@ const divide = (a,b) => {
 const one = 1;
 const two = 2;
 
-//crear una funcion que tome dos numeros y ejecute una funcion de operacion en ellos
+//create a operate function which uses a operator and two numbers to execute
 const operate = (a,b,operator) => {
     return operator(a,b);
 }
 console.log(operate(one,two,add))
+//add operator buttons
+const addBtn      = document.querySelector("#btn-add");
+const subtractBtn = document.querySelector("#subtract");
+const divideBtn   = document.querySelector("#divide");
+const multiplyBtn = document.querySelector("#multiply");
+const operateBtn  = document.querySelector("#result");
+const deleteBtn   = document.querySelector("#undo");
+const clearBtn    = document.querySelector("#delete");
+
+//add number buttons
+const oneBtn     = document.querySelector("#one");
+const twoBtn     = document.querySelector("#two");
+const threeBtn   = document.querySelector("#three");
+const forBtn     = document.querySelector("#for");
+const fiveBtn    = document.querySelector("#five");
+const sixBtn     = document.querySelector("#six");
+const sevenBtn   = document.querySelector("#seven");
+const eightBtn   = document.querySelector("#eight");
+const nineBtn    =  document.querySelector("#nine");
+const zeroBtn    = document.querySelector("#zero");
+const decimalBtn = document.querySelector("#decimal");
+//a list of the buttons
+const btnList = [oneBtn,twoBtn,threeBtn,forBtn,fiveBtn,sixBtn,sevenBtn,eightBtn,nineBtn,zeroBtn,decimalBtn];
+const operatorsList = [addBtn,subtractBtn,divideBtn,multiplyBtn,operateBtn,deleteBtn,clearBtn];
+
+console.log(btnList,operatorsList)
